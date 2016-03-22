237fd717a03d35804d1fe143649b20a56a026d7c
RoboLoCo <roboloco@team5338.com>
2016-03-22 03:21:02 -0400

    Implemented reading and writing to mqserver over network

--- src/mqserver/MQServer.cpp
+++ src/mqserver/MQServer.cpp
@@ -17,7 +17,7 @@ double MQServer::GetDouble(std::string name) {
 	return doubleMap[name];
 }
 
-long MQServer::GetLong(std::string name) {
+int64_t MQServer::GetLong(std::string name) {
 	return longMap[name];
 }
 
@@ -30,8 +30,17 @@ void MQServer::Set(std::string name, double value) {
 	doubleMap[name] = value;	// TODO: THIS IS NOT THREAD-SAFE
 }
 
-void MQServer::Set(std::string name, long value) {
+void MQServer::Set(std::string name, int64_t value) {
 	longMap[name] = value;	// TODO: THIS IS NOT THREAD-SAFE
+}
+
+void MQServer::PutString(std::string name, std::string value) {
+}
+
+void MQServer::PutDouble(std::string name, double value) {
+}
+
+void MQServer::PutLong(std::string name, int64_t value) {
 }
 
 // Format for mqserver messages
@@ -74,45 +83,124 @@ struct packetHeader {
 	uint16_t datalen;
 };
 
+struct getResponseHeader {
+	uint8_t status;
+	uint8_t dataType;
+	uint16_t datalen;
+};
+
+struct setResponseHeader {
+	uint8_t status;
+};
+
+uint8_t MQServer::parsePacket(zmq::message_t& request, zmq::message_t& reply) {
+	if (request.size() > sizeof(packetHeader)) {
+		packetHeader* p = (mqserver::packetHeader*) (request.data()); // Packet header is at start of data
+		uint8_t* bytes = (uint8_t*) (request.data()); // Lets us access data as a byte array
+		auto namelen = p->readHeader.namelen; // Grab the namelength from the packe header
+		switch (p->readHeader.type) { // switch over packet type
+		case 0: {
+			// Read
+			std::string name((char*) (&bytes[sizeof(p->readHeader)]), namelen); // Name string starts after read header
+			getResponseHeader r;
+			r.status = 0; // Nominal status is 0
+			r.dataType = p->readHeader.type; //
+			switch (p->readHeader.type) {
+			case 0: // Null
+				return 2;
+				break;
+			case 1: { // Long
+				r.datalen = 8;
+				int64_t val = GetLong(name);
+				request.rebuild(sizeof(r) + r.datalen);
+				memcpy(request.data(), &r, sizeof(r));
+				memcpy((char*) request.data() + sizeof(r), &val, sizeof(val));
+				break;
+			}
+			case 2: { // Double
+				r.datalen = 8;
+				double val = GetDouble(name);
+				request.rebuild(sizeof(r) + r.datalen);
+				memcpy(request.data(), &r, sizeof(r));
+				memcpy((char*) request.data() + sizeof(r), &val, sizeof(val));
+				break;
+			}
+			case 3: { // String
+				std::string val = GetString(name);
+				r.datalen = val.length();
+				request.rebuild(sizeof(r) + r.datalen);
+				memcpy(request.data(), &r, sizeof(r));
+				memcpy((char*) request.data() + sizeof(r), val.data(),
+						r.datalen);
+				break;
+			}
+			default: {
+				return 2;
+				break;
+			}
+			}
+			break;
+		}
+		case 1: {
+			// Write
+			std::string name((char*) (&bytes[sizeof(p)]), namelen); // Name string starts after write header
+			auto dataOffset = sizeof(p) + namelen; // Data starts after header and name
+			setResponseHeader h;
+			h.status = 0;
+			switch (p->dataType) {
+			case 0: { // Null
+				return 2;
+				break;
+			}
+			case 1: { // Long
+				int64_t val = (int64_t) bytes[dataOffset];
+				Set(name, val);
+				reply.rebuild(&h,sizeof(h));
+				break;
+			}
+			case 2: { // Double
+				double val = (double) bytes[dataOffset];
+				Set(name, val);
+				reply.rebuild(&h,sizeof(h));
+				break;
+			}
+			case 3: { // String
+				std::string val((char*) &bytes[dataOffset]);
+				Set(name, val);
+				reply.rebuild(&h,sizeof(h));
+				break;
+			}
+			default: {
+				return 2;
+				break;
+			}
+			}
+			break;
+		}
+		default: {
+			// Unknown packet type
+			return 1;
+			break;
+		}
+		}
+	}
+	return 0;
+}
+
 void MQServer::Run() {
 	sock->bind("tcp://*:5810");
+	sock->bind("inproc://mqserver"); //bind locally
 	while (running) {
-		zmq::message_t request;
+		zmq::message_t request, reply;
 
 		sock->recv(&request);
-		if (request.size() > sizeof(packetHeader)) {
-			packetHeader *p = (mqserver::packetHeader*) request.data();
-			uint8_t *bytes = (uint8_t*) request.data();
-			auto namelen = p->readHeader.namelen;
-			switch (p->readHeader.type) {
-			case 0: { // Read
-				std::string name((char*) &bytes[sizeof(p->readHeader)], namelen); // Name string starts after read header
-				switch (p->readHeader.type) {
-				case 0: // Null
-					break;
-				case 1: // Long
-					GetLong(name);
-					break;
-				case 2: // Double
-					GetDouble(name);
-					break;
-				case 3: // String
-					GetString(name);
-					break;
-				default:
-					break;
-				}
-				break;
-			}
-			case 1: {// Write
-				break;
-			}
-			default: {// Unknown packet type
-				break;
-			}
-			}
+		auto error = parsePacket(request, reply);
+		if (error != 0) {
+			reply.rebuild(2); // 2 byte error response, type=255, second byte is error code
+			((uint8_t*) reply.data())[0] = 255;
+			((uint8_t*) reply.data())[1] = error;
 		}
-
+		sock->send(reply);
 	}
 	sock->close();
 	context->close();
--- src/mqserver/MQServer.h
+++ src/mqserver/MQServer.h
@@ -28,13 +28,18 @@ class MQServer {
 	bool running = true;
 
 	void Run();
+	uint8_t parsePacket(zmq::message_t& request, zmq::message_t& reply);
+	void Set(std::string name, std::string value);
+	void Set(std::string name, double value);
+	void Set(std::string name, int64_t value);
+
 public:
 	std::string GetString(std::string name);
 	double GetDouble(std::string name);
-	long GetLong(std::string name);
-	void Set(std::string name, std::string value);
-	void Set(std::string name, double value);
-	void Set(std::string name, long value);
+	int64_t GetLong(std::string name);
+	void PutString(std::string name, std::string value);
+	void PutDouble(std::string name, double value);
+	void PutLong(std::string name, int64_t value);
 	MQServer();
 	virtual ~MQServer() = default;
 };
